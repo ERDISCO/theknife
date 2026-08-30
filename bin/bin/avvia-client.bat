b2cac59307c7aa5100d1d8d68123420f51c1016d
ERDISCO <eaarguetazepeda05@gmail.com>
2026-08-30 11:18:15 +0200

    Delete bin directory
    crazy caricando cartella te la crea

--- bin/bin/avvia-client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title TheKnife - Client
-cd /d "%~dp0"
-java -jar theknife-client-1.0.jar
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo [ERRORE] Il client si e' fermato con codice %ERRORLEVEL%.
-    pause
-)
--- bin/bin/avvia-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title TheKnife - Server
-cd /d "%~dp0"
-java -jar theknife-server-1.0.jar
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo [ERRORE] Il server si e' fermato con codice %ERRORLEVEL%.
-    pause
-)
